e0cf7c11ccc570c8ef728c4d27c36aea0bd9f123
ronshalit <shalit.ron@gmail.com>
2014-07-16 22:30:29 +0300

    Update app.js

--- blogjs/app.js
+++ blogjs/app.js
@@ -44,15 +44,6 @@ app.get("/",function(req,res){
 app.use('/', express.static(path.join(__dirname, 'public'))); 
 
 
-
-// single page application content
-/*spa.start(app,{
-	viewsPath:'./public/views/',
-	layoutFilename:'layout.htm', 
-	scriptsPath:'public/javascripts/',
-	stylesPath:'public/stylesheets/'
-});*/
-
 /// catch 404 and forwarding to error handler (nothing should get here- the SPA will catch everything)
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
