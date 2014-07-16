4fe5d705ef2723c6f1b72e8b5314380b02689ca8
ronshalit <shalit.ron@gmail.com>
2014-07-16 22:37:44 +0300

    Update PostDetail.js

--- blogjs/public/javascripts/Controllers/PostDetail.js
+++ blogjs/public/javascripts/Controllers/PostDetail.js
@@ -1,8 +1,11 @@
-﻿(function(){
+(function(){
     angular.module('spaControllers').controller('PostDetailCtrl', ['$scope', '$routeParams', 'Post',
       function ($scope, $routeParams, Post) {
-        $scope.rows = Post.toRows; 
+        
 	    $scope.postUrl = $routeParams.postUrl;
         $scope.post=Post.get({postUrl: $routeParams.postUrl});
+        $scope.html = function(c){return !c?"":Post.html(c)}
+        $scope.date = function(d){return new Date(d).toDateString()}       
+        
     }]);
 })();
