72c26a86a30725c0e7796ed48a314bede6a335db
ronshalit <shalit.ron@gmail.com>
2014-07-16 22:31:53 +0300

    added modification date

--- blogjs/middleware/controllers/posts.js
+++ blogjs/middleware/controllers/posts.js
@@ -1,9 +1,12 @@
-﻿var fs = require('fs');
+var fs = require('fs');
 var firstLine = /(.*?)(\n|\r)/;
 
 var posts = [
     //{url:"foo", title:"Foo", content:"bar"}
 ]
+
+var dates=[];
+var date2post={};
 
 var path='./BlogPosts/';
 // read the blog posts from the files in the BlogPosts directory
@@ -13,8 +16,15 @@ for(var i = 0; i<files.length; i++){
     var content=String.prototype.concat.call(fs.readFileSync(path+url),"");
     // extract the first line as the title
     var title = content.match(firstLine)[1];
-    content = content.replace(firstLine,'');
-    posts.push({url:url, title:title, content:content});
+    content = content.replace(firstLine,'').trim();
+    var date= fs.statSync(path+url).mtime;
+    date2post[date+url]={url:url, title:title, content:content, date:date};
+    dates.push(date+url);
+}
+// sort posts by date descending
+dates = dates.sort().reverse();
+for(var i=0; i<dates.length; i++){
+    posts[i]=date2post[dates[i]];
 }
 
 // keep the posts in a dictionary, too, for easy extractions
