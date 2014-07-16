d07721014789d3de7229976c630591c345151e8e
ronshalit <shalit.ron@gmail.com>
2014-07-16 22:37:17 +0300

    Update Posts.js

--- blogjs/public/javascripts/Controllers/Posts.js
+++ blogjs/public/javascripts/Controllers/Posts.js
@@ -1,7 +1,11 @@
-﻿(function(){
+(function(){
     angular.module('spaControllers').controller('PostsCtrl', ['$scope', '$routeParams', 'Post',
     function ($scope, $routeParams, Post) {
-        $scope.firstTwoRows = function (s) { return Post.toRows(s).slice(0, 2) };
-        $scope.posts = Post.query();        
+        $scope.posts = Post.query(); 
+        $scope.firstRow= function(p){ 
+            var firstRow = p.match(/(.*)(\r|\n)/);
+            return Post.html(!firstRow?p:firstRow[0])
+        }
+        $scope.date = function(d){return !d?"": new Date(d).toDateString()}
     }]);
 })();
